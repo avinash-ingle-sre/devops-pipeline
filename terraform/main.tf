2636e473b6e875dbf66f51b4edbdd09856d73f2d
avinash-ingle-sre <avinash.ingle.sre@gmail.com>
2025-06-12 09:36:04 +0000

    ♻️ Trigger apply from workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,34 @@
-provider "aws" {
-  region = "ap-south-1"
+# Define variables
+variable "aws_region" {
+  default = "ap-south-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc"
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  default = "main-vpc"
+}
+
+# AWS provider block
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Owner       = "avinash"
+      Environment = "dev"
+    }
   }
 }
+
+# VPC resource block
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
